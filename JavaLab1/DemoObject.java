9363a5f72990e51ce938c35345cc5f12f80973cf
Pradeep Yadav <pradpyadav@gmail.com>
2020-11-16 14:27:46 -0600

    Created DemoObject java

---
+++ JavaLab1/DemoObject.java
@@ -0,0 +1,18 @@
+class DemoObject{
+
+public String name;
+
+public DemoObject(){
+
+}
+
+public String getName(){
+  return this.name;
+}
+
+
+public void setName(String n){
+  this.name=n;
+}
+
+}
